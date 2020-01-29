5e2490dcd47f246065952b360ad4bf8e3ed80bfc
Igor Souza <igormcsouza@gmail.com>
2020-01-29 20:38:12 -0300

    small bugs fixed

--- imageclassification/io/hdf5datasetwriter.py
+++ imageclassification/io/hdf5datasetwriter.py
@@ -23,39 +23,39 @@ class HDF5DatasetWriter:
         self.buffer = {"data": [], "labels": []}
         self.idx = 0
 
-        def add(self, rows, labels):
-            # add the rows and labels to the buffer
-            self.buffer["data"].extend(rows)
-            self.buffer["labels"].extend(labels)
+    def add(self, rows, labels):
+        # add the rows and labels to the buffer
+        self.buffer["data"].extend(rows)
+        self.buffer["labels"].extend(labels)
 
-            # check to see if the buffer needs to be flushed to disk
-            if len(self.buffer["data"]) >= self.bufSize:
-                self.flush()
+        # check to see if the buffer needs to be flushed to disk
+        if len(self.buffer["data"]) >= self.bufSize:
+            self.flush()
 
-        def flush(self):
-            # write the buffers to disk then reset the buffer
-            i = self.idx + len(self.buffer["data"])
-            self.data[self.idx:i] = self.buffer["data"]
-            self.labels[self.idx:i] = self.buffer["labels"]
-            self.idx = i
-            self.buffer = {"data": [], "labels": []}
+    def flush(self):
+        # write the buffers to disk then reset the buffer
+        i = self.idx + len(self.buffer["data"])
+        self.data[self.idx:i] = self.buffer["data"]
+        self.labels[self.idx:i] = self.buffer["labels"]
+        self.idx = i
+        self.buffer = {"data": [], "labels": []}
 
-        def storeClassLabels(self, classLabels):
-            # create a dataset to store the actual class label names,
-            # then store the class labels
-            dt = h5py.special_dtype(vlen=unicode)
-            labelSet = self.db.create_dataset(
-                "label_names", (len(classLabels),), dtype=dt
-            )
-            labelSet[:] = classLabels
+    def storeClassLabels(self, classLabels):
+        # create a dataset to store the actual class label names,
+        # then store the class labels
+        dt = h5py.special_dtype(vlen=str)
+        labelSet = self.db.create_dataset(
+            "label_names", (len(classLabels),), dtype=dt
+        )
+        labelSet[:] = classLabels
 
-        def close(self):
-            # check to see if there are any other entries in the buffer
-            # that need to be flushed to disk
-            if len(self.buffer["data"]) > 0:
-                self.flush()
+    def close(self):
+        # check to see if there are any other entries in the buffer
+        # that need to be flushed to disk
+        if len(self.buffer["data"]) > 0:
+            self.flush()
 
-            # close the dataset
-            self.db.close()
+        # close the dataset
+        self.db.close()
 
         
